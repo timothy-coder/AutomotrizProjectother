a8c2dfc1f27df4e8671446a12ab4f5e1dc39ff71
timothy-coder <nicopar666xd@gmail.com>
2026-03-03 09:01:11 -0500

    Update page.jsx

---
+++ app/(dashboard)/agenda/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+import {
+    format,
+    addWeeks,
+    subWeeks,
+    startOfWeek,
+    addDays
+} from "date-fns";
+import { es } from "date-fns/locale";
+
+import { Button } from "@/components/ui/button";
+import {
+    Select,
+    SelectTrigger,
+    SelectValue,
+    SelectContent,
+    SelectItem
+} from "@/components/ui/select";
+
+export default function AgendaPage() {
+    return(
+        <>hola</>
+    )
+}
